3bae6e6656815991a07d618e7ba02008410fc6f9
zhuchenhang1 <2574284191@qq.com>
2020-06-23 12:01:47 +0800

    百万富翁

---
+++ evryday/Day13second.java
@@ -0,0 +1,11 @@
+package practice;
+//百万富翁
+public class Day13second {
+    public static void main(String[] args) {
+int strange=0;
+int rich=0;
+strange=10*30;//万元
+        rich=(int)(Math.pow(2,30)-1);
+        System.out.print(strange+" "+rich);
+    }
+}
